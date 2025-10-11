805f42b23f6be204d6beeb531635b528610ce4af
Shawn Marlin <marlins@leidoscloud.com>
2025-10-10 18:18:33 -1000

    Deal with postfix

--- StatePipes/Comms/ServiceConfiguration.cs
+++ StatePipes/Comms/ServiceConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using StatePipes.Common;
 using StatePipes.ProcessLevelServices;
 namespace StatePipes.Comms
 {
@@ -14,7 +15,30 @@ namespace StatePipes.Comms
             ContainerSetupClassLibraryTypeFullName = containerSetupClassLibraryTypeFullName;
             ProxySubstitutions = proxySubstitutions;
         }
-        public void MergeCommandLineArgs(ServiceArgs args) => Args.Merge(args);
+        internal void MergeCommandLineArgs(ServiceArgs args)
+        {
+            Args.Merge(args);
+            foreach (var proxyConfig in this.ProxyConfigurations)
+            {
+                proxyConfig.ServiceConfiguration.MergeCommandLineArgs(args);
+            }
+        }
+        internal void AddPostfixWorker(string? postfix, bool recursiveAddToProxies)
+        {
+            if (string.IsNullOrEmpty(postfix)) return;
+            this.BusConfig.SetExchangeNamePostfix(BusConfig.ExchangeNamePostfix + postfix);
+            if (!recursiveAddToProxies) return;
+            foreach (var proxyConfig in this.ProxyConfigurations)
+            {
+                proxyConfig.ServiceConfiguration.AddPostfixWorker(postfix, recursiveAddToProxies);
+            }
+        }
+        public ServiceConfiguration AddPostfix(string postfix, bool recursiveAddToProxies = false)
+        {
+            ServiceConfiguration clonedServiceConfiguration = JsonUtility.Clone(this)!;
+            clonedServiceConfiguration.AddPostfixWorker(postfix, recursiveAddToProxies);
+            return clonedServiceConfiguration;
+        }
         public BusConfig BusConfig { get; }
         public string AssemblyName { get; }
         public string ContainerSetupClassLibraryTypeFullName { get; }
