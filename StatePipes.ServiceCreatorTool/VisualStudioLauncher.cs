0a6ef235f68385456be2cea55aa13300180932dd
Shawn Marlin <marlins@leidoscloud.com>
2025-11-24 07:09:29 -1000

    Converted to .Net10

--- StatePipes.ServiceCreatorTool/VisualStudioLauncher.cs
+++ StatePipes.ServiceCreatorTool/VisualStudioLauncher.cs
@@ -5,36 +5,40 @@ namespace StatePipes.ServiceCreatorTool
 {
     internal class VisualStudioLauncher
     {
-        public static void LaunchSolution(string solutionFullPath) => LaunchVsDte(isPreRelease: false, arguments: solutionFullPath);
-        private static void LaunchVsDte(bool isPreRelease, string arguments)
+        public static void LaunchSolution(string solutionFullPath) => LaunchVsDte(arguments: solutionFullPath);
+        private static void LaunchVsDte(string arguments)
         {
-            ISetupInstance setupInstance = GetSetupInstance(isPreRelease);
+            ISetupInstance? setupInstance = GetLatest();
+            if (setupInstance == null) return;
             string installationPath = setupInstance.GetInstallationPath();
             string executablePath = Path.Combine(installationPath, @"Common7\IDE\devenv.exe");
             Process vsProcess = Process.Start(executablePath, arguments);
         }
-        private static ISetupInstance GetSetupInstance(bool isPreRelease) =>  GetSetupInstances().First(i => IsPreRelease(i) == isPreRelease);
+        private static ISetupInstance? GetLatest()
+        {
+            var instances = GetSetupInstances();
+            if (instances.Count() <= 0) return null;
+            return instances.OrderByDescending(i => new Version(i.GetInstallationVersion())).First();
+        }
         private static IEnumerable<ISetupInstance> GetSetupInstances()
         {
+            List<ISetupInstance> instances = new List<ISetupInstance>();
             ISetupConfiguration setupConfiguration = new SetupConfiguration();
             IEnumSetupInstances enumerator = setupConfiguration.EnumInstances();
-
             int count;
             do
             {
                 ISetupInstance[] setupInstances = new ISetupInstance[1];
                 enumerator.Next(1, setupInstances, out count);
-                if (count == 1 && setupInstances[0] != null)
+                if (count == 1 && setupInstances[0] != null && !IsPreRelease(setupInstances[0]))
                 {
-                    yield return setupInstances[0];
+                    instances.Add(setupInstances[0]);
                 }
             }
             while (count == 1);
+            return instances;
         }
-        private static bool IsPreRelease(ISetupInstance setupInstance)
-        {
-            ISetupInstanceCatalog setupInstanceCatalog = (ISetupInstanceCatalog)setupInstance;
-            return setupInstanceCatalog.IsPrerelease();
-        }
+        private static bool IsPreRelease(ISetupInstance setupInstance) => ((ISetupInstanceCatalog)setupInstance).IsPrerelease();
     }
 }
+ 
